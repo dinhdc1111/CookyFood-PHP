a03fecb8fc3d731f78024eb33dd496cf0e11e3ec
Dong Cong Dinh <dinhdc1111@gmail.com>
2023-10-08 20:49:10 +0700

    feat: :sparkles: upload avatar from account manager

--- admin/account/list.php
+++ admin/account/list.php
@@ -17,15 +17,13 @@ include('./common.php');
         <table class="table">
             <thead>
                 <tr class="text-center">
-                    <th class="font-weight-bold w-20px" scope="col">#</th>
                     <th class="font-weight-bold" scope="col">ID</th>
                     <th class="font-weight-bold" scope="col">Tên người dùng</th>
+                    <th class="font-weight-bold" scope="col">Ảnh đại diện</th>
                     <th class="font-weight-bold" scope="col">Email</th>
-                    <th class="font-weight-bold" scope="col">Số điện thoại</th>
+                    <th class="font-weight-bold" scope="col">Điện thoại</th>
                     <th class="font-weight-bold" scope="col">Địa chỉ</th>
                     <th class="font-weight-bold" scope="col">Vai trò</th>
-                    <th class="font-weight-bold" scope="col">Ngày tạo</th>
-                    <th class="font-weight-bold" scope="col">Ngày cập nhật</th>
                     <th class="font-weight-bold" scope="col">Trạng thái</th>
                     <th class="font-weight-bold" scope="col">Hành động</th>
                 </tr>
@@ -34,29 +32,22 @@ include('./common.php');
                 <?php foreach ($list_account as $account) : ?>
                     <?php
                     extract($account);
-                    $formatted_date_create = date('d/m/Y', strtotime($created_at));
-                    $formatted_date_update = ($updated_at == "0000-00-00 00:00:00")
-                        ? "<small class='text-primary'>Chưa cập nhật</small>"
-                        : date('d/m/Y', strtotime($updated_at));
-                    
                     $formatRole = ($role == 1)
-                        ? "<strong class='text-danger text-uppercase'>Quản trị viên</strong>"
-                        : "<small class='text-primary text-uppercase'>Người dùng</small>" 
+                        ? "<strong class='text-danger text-uppercase'>Admin</strong>"
+                        : "<strong class='text-primary text-uppercase'>User</strong>";
+                    $pathImage = "../uploads/" . $image;
+                    $showImage = is_file($pathImage)
+                        ? "<img class='border rounded' src='{$pathImage}' alt='{$username}'height='100' width='100' style='object-fit: cover'/>"
+                        : "<img class='border rounded' src='https://res.cloudinary.com/do9rcgv5s/image/upload/v1695895241/cooky%20market%20-%20PHP/itcq4ouly2zgyzxqwmeh.jpg' alt='Không có ảnh' height='100' width='100'>";
                     ?>
                     <tr class="text-center">
-                        <td scope="row">
-                            <div class="form-check">
-                                <input class="form-check-input" type="checkbox" value="">
-                            </div>
-                        </td>
                         <th><?= $id ?></th>
                         <td class="text-primary"><?= $username ?></td>
+                        <td><?= $showImage ?></td>
                         <td><?= $email ?></td>
                         <td><?= $phone ?></td>
                         <td><?= $address ?></td>
                         <td><?= $formatRole ?></td>
-                        <td><?= $formatted_date_create ?></td>
-                        <td><?= $formatted_date_update ?></td>
                         <td class="text-success">Hoạt động</td>
                         <td>
                             <a href="index.php?req=account-delete-soft&id=<?= $id ?>" title="Xóa" class="btn btn-outline-danger btn-sm border border-0 delete-account-button" data-account-id="<?= $id ?>"><i class="fa-regular fa-trash-can"></i></a>
--- admin/account/update.php
+++ admin/account/update.php
@@ -3,6 +3,8 @@ include('./common.php');
 if (is_array($account)) {
     extract($account);
 }
+// Preview image
+$pathImage = isset($image) ? "../uploads/{$image}" : "https://res.cloudinary.com/do9rcgv5s/image/upload/v1695895241/cooky%20market%20-%20PHP/itcq4ouly2zgyzxqwmeh.jpg";
 ?>
 <div class="p-4">
     <nav aria-label="breadcrumb">
@@ -14,11 +16,18 @@ if (is_array($account)) {
     <a class="text-light text-decoration-none btn btn-primary btn-sm mb-3" href="index.php?req=account">
         <i class="fa-solid fa-list-ul"></i> Danh sách người dùng
     </a>
-    <form action="index.php?req=account-update" method="POST">
+    <form action="index.php?req=account-update" method="POST" enctype="multipart/form-data">
         <div class="form-group">
             <input type="hidden" name="id" id="id" class="form-control" value="<?= isset($id) && $id > 0 ? $id : '' ?>">
             <label for="username">Tên người dùng</label>
             <input type="text" name="username" id="username" class="form-control form-control-sm" value="<?= $username ?>">
+        </div>
+        <div class="form-group">
+            <img class='border rounded' id="preview-image" src="<?= $pathImage ?>" alt="<?= $name ?>" height='115' width='115' style='object-fit: cover'>
+            <input class="form-control form-control-sm d-none" type="file" id="image" name="image" onchange="previewImage(this)">
+            <label for="image" class="form-label label-for-file mt-3">
+                <i class="fa-solid fa-file-image"></i>&nbsp;Chọn ảnh
+            </label>
         </div>
         <div class="form-group">
             <label for="email">Email</label>
@@ -40,3 +49,19 @@ if (is_array($account)) {
         <input type="submit" class="btn btn-primary btn-block" name="submit" value="CẬP NHẬT" />
     </form>
 </div>
+<script>
+    function previewImage(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                $('#preview-image').attr('src', e.target.result);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+    $(document).ready(function() {
+        $('#image').on('change', function() {
+            previewImage(this);
+        });
+    });
+</script>
